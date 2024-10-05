955f80ad872f3c3dfcef3ef4ddacfd82dc26ee60
Nattapong Promthong <banker.z412@gmail.com>
2024-10-05 19:33:18 +0700

    feat: try to get value from localStorage

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [storedValue, setStoredValue] = useState("No value");
+  useEffect(() => {
+    const value = localStorage.getItem("myValue");
+    if (value) {
+      setStoredValue(value);
+    }
+  }, []);
+
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <p className="mt-4 text-xl">Stored Value: {storedValue}</p>
+    </div>
   );
 }
 
