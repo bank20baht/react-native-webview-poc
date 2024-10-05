57e4781c8e70925d2f02edc52e1a8b07267d4e96
Nattapong Promthong <banker.z412@gmail.com>
2024-10-05 19:46:49 +0700

    feat: prepare website for webview

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [storedValue, setStoredValue] = useState("No value");
+
   useEffect(() => {
     const value = localStorage.getItem("myValue");
     if (value) {
@@ -10,12 +11,19 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
-      <p className="mt-4 text-xl">Stored Value: {storedValue}</p>
-    </div>
+    <>
+      <div className="flex flex-col gap-2 justify-center items-center w-full min-h-screen">
+        <h1 className="text-6xl font-bold text-center">
+          React Native Webview POC
+        </h1>
+        <div className="text-2xl text-center">
+          Try passing value from mobile to Web application
+        </div>
+        <p className="mt-4 text-2xl text-center">
+          Stored Value From Mobile: {storedValue}
+        </p>
+      </div>
+    </>
   );
 }
 
